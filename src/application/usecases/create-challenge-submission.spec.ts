fa7120f88b2139fe3f04b252fd66e02c1e02c218
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-14 23:58:06 -0300

    Update create-challenge-submission.spec.ts

--- src/application/usecases/create-challenge-submission.spec.ts
+++ src/application/usecases/create-challenge-submission.spec.ts
@@ -1,16 +1,32 @@
 import { InMemoryChallengesRepository } from "../../../tests/repositories/in-memory-challenges-repository";
 import { InMemoryStudentsRepository } from "../../../tests/repositories/in-memory-students-repository";
+import { Challenge } from "../../domain/entities/challenge";
+import { Student } from "../../domain/entities/student";
 import { CreateChallengeSubmission } from "./create-challenge-submission";
 
 describe('Create challenge submission use case', () => {
   it('should be able to create a new challenge submission', async () => {
     const studentsRepository = new InMemoryStudentsRepository();
     const challengesRepository = new InMemoryChallengesRepository();
+
+    const student = Student.create({
+      name: 'Ruan',
+      email: 'ruan@email.com',
+    });
+
+    const challenge = Challenge.create({
+      title: 'Create challenge node js',
+      instructionUrl: 'http://localhost.com',
+    });
+
+    studentsRepository.items.push(student);
+    challengesRepository.items.push(challenge);
+
     const sut = new CreateChallengeSubmission(studentsRepository, challengesRepository);
 
     const response = await sut.execute({
-      studentId: 'fake-student-id',
-      challengeId: 'fake-challenge-id'
+      studentId: student.id,
+      challengeId: challenge.id
     });
 
     expect(response).toBeTruthy();
