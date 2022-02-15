9c6d141e3248bc97c432b7acd51710c60adf2f6e
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-14 23:47:35 -0300

    More tests

--- src/application/usecases/create-challenge-submission.ts
+++ src/application/usecases/create-challenge-submission.ts
@@ -1,4 +1,6 @@
 import { Submission } from "../../domain/entities/submission";
+import { ChallengesRepository } from "../repositories/ChallengesRepository";
+import { StudentRepository } from "../repositories/StudentRepository";
 
 type CreateChallengeSubmissionProps = {
   studentId: string;
@@ -6,7 +8,24 @@ type CreateChallengeSubmissionProps = {
 }
 
 export class CreateChallengeSubmission {
+  constructor(
+    private studentRepository: StudentRepository,
+    private challengesRepository: ChallengesRepository
+  ) { }
+
   async execute({ studentId, challengeId }: CreateChallengeSubmissionProps) {
+    const student = await this.studentRepository.findById(studentId);
+
+    if (!student) {
+      throw new Error('Student does not exists.')
+    }
+
+    const challenge = await this.challengesRepository.findById(challengeId);
+
+    if (!challenge) {
+      throw new Error('Challenge does not exists.')
+    }
+
     const submission = Submission.create({
       studentId,
       challengeId,
