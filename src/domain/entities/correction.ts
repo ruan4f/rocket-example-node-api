ad1a2d61eead63e025cff9964902c763e4c88f46
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-14 23:15:50 -0300

    new entities

--- src/domain/entities/correction.ts
+++ src/domain/entities/correction.ts
@@ -1,3 +1,23 @@
-class Correction {
+import { Entity } from "../../core/domain/Entity";
 
+type CorrectionProps = {
+  grade: number;
+  submissionId: number;
+  createdAt: number;
+}
+
+class Correction  extends Entity<CorrectionProps> {
+  private constructor(props: CorrectionProps, id?: string) {
+    super(props, id);
+  }
+
+  static create(props: CorrectionProps, id?: string) {
+    // if (props.grade < 0 || props.grade > 10) {
+    //   throw new Error('');
+    // }
+    
+    const correction = new Correction(props, id);
+
+    return correction;
+  }
 }
