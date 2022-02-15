6a24d0a25cfa1067c56288951c4b9af7310ccea6
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-14 23:53:39 -0300

    Test in memory for repositories

--- src/application/usecases/create-challenge-submission.spec.ts
+++ src/application/usecases/create-challenge-submission.spec.ts
@@ -1,8 +1,12 @@
+import { InMemoryChallengesRepository } from "../../../tests/repositories/in-memory-challenges-repository";
+import { InMemoryStudentsRepository } from "../../../tests/repositories/in-memory-students-repository";
 import { CreateChallengeSubmission } from "./create-challenge-submission";
 
 describe('Create challenge submission use case', () => {
   it('should be able to create a new challenge submission', async () => {
-    const sut = new CreateChallengeSubmission();
+    const studentsRepository = new InMemoryStudentsRepository();
+    const challengesRepository = new InMemoryChallengesRepository();
+    const sut = new CreateChallengeSubmission(studentsRepository, challengesRepository);
 
     const response = await sut.execute({
       studentId: 'fake-student-id',
